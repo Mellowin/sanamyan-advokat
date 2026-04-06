85219ac78398b06d00825e38860a207ce9cf378e
Mellow <mellowin1987@gmail.com>
2026-04-06 13:55:32 +0300

    Fix Google Analytics ID to G-KEWBKKHE13

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
     <>
       {/* Google Analytics */}
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-KEM8KKHE13"
+        src="https://www.googletagmanager.com/gtag/js?id=G-KEWBKKHE13"
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -17,7 +17,7 @@ export default function RootLayout({
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-KEM8KKHE13');
+          gtag('config', 'G-KEWBKKHE13');
         `}
       </Script>
       {children}
