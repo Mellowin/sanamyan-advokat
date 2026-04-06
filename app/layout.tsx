f6b97869e7ba4defd653c4bade032e80cb74968c
Mellow <mellowin1987@gmail.com>
2026-04-06 13:50:45 +0300

    Add Google Analytics (G-KEM8KKHE13)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,26 @@
+import Script from 'next/script';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return children;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KEM8KKHE13"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KEM8KKHE13');
+        `}
+      </Script>
+      {children}
+    </>
+  );
 }
